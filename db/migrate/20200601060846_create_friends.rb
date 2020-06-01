0347e047837a101f933e0ffcb854d1f03eaa5991
Georgie Brown <georginaabrown@gmail.com>
2020-06-01 16:18:49 +1000

    added friend model

---
+++ db/migrate/20200601060846_create_friends.rb
@@ -0,0 +1,12 @@
+class CreateFriends < ActiveRecord::Migration[6.0]
+  def change
+    create_table :friends do |t|
+      t.string :name
+      t.string :description
+      t.string :location
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
