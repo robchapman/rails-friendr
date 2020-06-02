27756f31e9aecff16db3557353d67cb994a601d5
Robert Chapman <robchapman94@gmail.com>
2020-06-02 16:06:26 +1000

    new routes and actions for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'dashboard' to: 'pages#dashboard'
-  get 'my_bookings' to: 'pages#my_bookings' #Bookings summary page for consumer
-  get 'my_listings' to: 'pages#my_listings'
+  get 'dashboard', to: 'pages#dashboard'
+  get 'my_bookings', to: 'pages#my_bookings' #Bookings summary page for consumer
+  get 'my_listings', to: 'pages#my_listings'
   resources :friends do
     resources :bookings , only: [:create]
     member do
