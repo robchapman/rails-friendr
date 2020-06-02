c5eba080ceee4ab7a8f520f28a0454a1e51bf29e
Georgie Brown <georginaabrown@gmail.com>
2020-06-03 08:51:53 +1000

    updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #  "name" "description" "location" t.integer "price"
-Friend.create(name: "A Mans Best Friend", description: "A loyal labrador who will fetch your paper and follow you around the house", location: "South Yarra", price: "30")
-Friend.create(name: "Betty", description: "Grandma, she will knit you socks and bake you cakes", location: "Lovely Hill", price: "70")
+
+Friend.create(name: "A Mans Best Friend", description: "A loyal labrador who will fetch your paper and follow you around the house", location: "South Yarra", price: "30" )
+Friend.create(name: "Betty", description: "Grandma, she will knit you socks and bake you cakes", location: "Lovely Hill", price: "70", photo_url: "")
 Friend.create(name: "Marty", description: "The take anywhere friend. Great with parents, life of the party", location: "Heaven", price: "40")
