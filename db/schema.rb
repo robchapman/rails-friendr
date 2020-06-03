45602e00be04a975d80de8c2dffeeaa87b6c38f4
Georgie Brown <georginaabrown@gmail.com>
2020-06-03 13:53:35 +1000

    cloudinary fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_052713) do
+ActiveRecord::Schema.define(version: 2020_06_03_033527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_052713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.string "img_url"
     t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
@@ -109,6 +110,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_052713) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "img_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
