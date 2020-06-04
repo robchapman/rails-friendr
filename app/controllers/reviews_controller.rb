2a6a8e581cb04a7b722d5f46f5de69ae9ed9b98d
Nesil Ozer <nesilozr@gmail.com>
2020-06-04 14:27:21 +1000

    adding reviews to friend page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+      if @review.save
+        redirect_to booking_path(@booking)
+      else
+        render 'new'
+      end
+   end
+
+   private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
