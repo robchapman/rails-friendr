ac6442ab4778d4c1a5452ab07808d31f89b7609f
Robert Chapman <robchapman94@gmail.com>
2020-06-04 21:45:08 +1000

    Added USer profile Edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  # get 'user_profile/:id', to: 'users#show'
+  get 'user_profile/:id/edit', to: 'users#edit', as: :edit_user_profile
+  patch 'user_profile/:id', to: 'users#update', as: :user_profile
   root to: 'pages#home'
   get 'dashboard', to: 'pages#dashboard'
   get 'my_bookings', to: 'pages#my_bookings' #Bookings summary page for consumer
@@ -17,5 +19,5 @@ Rails.application.routes.draw do
     resources :reviews ,  only: [:new, :create]
    end
 
-    #ADD DELETE AND EDIT BOOKING LATER
+
 end
